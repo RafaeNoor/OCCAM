ddb9424801a3a23d96a6c8905166cea8176eae57
Abdul Rafae Noor <abdurrafae98@live.com>
2020-07-23 03:41:37 -0700

    Pass to identify external functions inside a module

---
+++ src/transforms/GetExternalFunctions.cpp
@@ -0,0 +1,97 @@
+/**
+ * LLVM transformation pass to resolve indirect calls
+ *
+ * The transformation performs "devirtualization" which consists of
+ * looking for indirect function calls and transforming them into a
+ * switch statement that selects one of several direct function calls
+ * to execute. Devirtualization happens if a pointer analysis can
+ * resolve the indirect calls and compute all possible callees.
+ **/
+
+#include <iostream>
+#include <fstream>
+
+#include "llvm/Pass.h"
+//#include "llvm/Analysis/CallGraph.h"
+#include "llvm/IR/MDBuilder.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "seadsa/CompleteCallGraph.hh"
+#include "seadsa/InitializePasses.hh"
+
+
+namespace previrt {
+    namespace transforms {
+
+        using namespace llvm;
+
+        /** 
+         ** Resolve indirect calls by one direct call for possible callee
+         ** function 
+         **/
+        class GetExternalFunctions : public ModulePass {
+            public:
+                static char ID;
+
+                GetExternalFunctions() : ModulePass(ID) {
+                    // Initialize sea-dsa pass
+                    //llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
+                    //llvm::initializeCompleteCallGraphPass(Registry);
+                }
+
+                virtual bool runOnModule(Module &M) override {
+                    std::ofstream write_file;
+                    write_file.open("external.functions");
+                    write_file << "{ functions: [";
+                    std::vector<std::string> functions;
+
+                    for(Function &F: M){
+                        if(F.isDeclaration()){
+                            errs()<<F.getName()<<":\tDeclaration"<<"\n";
+                            functions.push_back(F.getName());
+                        }
+                    }
+
+
+                    for(int i; i < functions.size(); ++i){
+                        write_file << "\""<<(functions[i])<<"\"";
+
+                        if(i != (functions.size()-1)){
+                            write_file <<", ";
+                        }
+                    }
+
+                    write_file << "] }";
+
+                    write_file.close();
+
+
+                    return false;
+                }
+
+                virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
+                    // AU.addRequired<CallGraphWrapperPass>();
+                    //AU.addRequired<seadsa::CompleteCallGraph>();
+                    // FIXME: DevirtualizeFunctions does not fully update the call
+                    // graph so we don't claim it's preserved.
+                    // AU.setPreservesAll();
+                    // AU.addPreserved<CallGraphWrapperPass>();
+                }
+
+                virtual StringRef getPassName() const override {
+                    return "Remove Main Function from Bitcode";
+                }
+        };
+
+        char GetExternalFunctions::ID = 0;
+
+    } // end namespace
+} // end namespace
+
+static llvm::RegisterPass<previrt::transforms::GetExternalFunctions>
+X("GetExternal",
+        "Get the name of all external functions within a module",
+        false,
+        false);
+
