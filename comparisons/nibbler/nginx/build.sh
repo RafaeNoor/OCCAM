cb349266182f57c8ec8af538d6b2a82ce7d7c0f9
Abdul Rafae Noor <abdurrafae98@live.com>
2020-08-13 06:47:12 -0700

    Printing stats after library specialization

--- comparisons/nibbler/nginx/build.sh
+++ comparisons/nibbler/nginx/build.sh
@@ -44,7 +44,7 @@ EOF
 export OCCAM_LOGLEVEL=INFO
 export OCCAM_LOGFILE=${PWD}/slash/occam.log
 
-slash --no-strip --intra-spec-policy=onlyonce --work-dir=slash  manifest
+slash --no-strip --intra-spec-policy=onlyonce --stats --work-dir=slash  manifest
 
 #debugging stuff below:
 for bitcode in slash/*.bc; do
